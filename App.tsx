ae0ba2af3ec36cb7e50064ef31cdeea68efbf45f
pranaya sharma <pranayasharma8@gmail.com>
2025-03-09 08:07:33 -0600

    First push

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginPage from 'components/(auth)/LoginPage';
+import SignupPage from 'components/(auth)/SignupPage';
+import HomeScreen from 'components/HomeScreen';
+import BottomTabs from 'navigation/BottomTabs';
 
-import './global.css';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: '#0f172a' }, // Deep Blue
+          headerTintColor: '#FFF', // Ensures text/icons are readable
+        }}>
+        <Stack.Screen
+          name="Login"
+          component={LoginPage}
+          options={{
+            title: 'Login',
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="Signup" component={SignupPage} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="(tabs)" component={BottomTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
